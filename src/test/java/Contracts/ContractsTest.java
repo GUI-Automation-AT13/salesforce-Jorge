13a9cb2eed540bed796f4ef82d619e13c8283cdf
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-07-19 10:04:43 -0400

    Add page objects and test with full valid values

--- src/test/java/Contracts/ContractsTest.java
+++ src/test/java/Contracts/ContractsTest.java
@@ -4,7 +4,6 @@ import base.BaseTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import salesforce.config.EnvConfig;
-import salesforce.ui.PageTransporter;
 import salesforce.ui.pages.HomePage;
 import salesforce.ui.pages.LoginPage;
 import salesforce.ui.pages.contracts.ContractsPage;
@@ -19,7 +18,7 @@ public class ContractsTest extends BaseTests {
     private CreatedContractPage createdContractPage;
 
     @Test
-    public void createContractTest()  {
+    public void createContractWithRequiredValuesTest()  {
         LoginPage loginPage = new LoginPage();
         String accountName = "TestAccount";
         homePage = loginPage.loginSuccessful(EnvConfig.getInstance().getUser(),
@@ -30,6 +29,35 @@ public class ContractsTest extends BaseTests {
         newContractPage.setAccountName(accountName);
         newContractPage.setContractTermMonths("2");
         newContractPage.setContractStartDate("7/15/2021");
+        createdContractPage = newContractPage.clickSave();
+        Assert.assertEquals(createdContractPage.getAccountName(), "TestAccount");
+    }
+    @Test
+    public void createContractWithAllValuesTest()  {
+        LoginPage loginPage = new LoginPage();
+        String accountName = "TestAccount";
+        homePage = loginPage.loginSuccessful(EnvConfig.getInstance().getUser(),
+                EnvConfig.getInstance().getPassword());
+        contractsPage = pageTransporter.navigateToContractsPage();
+        contractsPage.clickPopUP();
+        newContractPage = contractsPage.clickNew();
+        newContractPage.setAccountName(accountName);
+        newContractPage.setContractTermMonths("2");
+        newContractPage.setContractStartDate("7/15/2021");
+        newContractPage.setCustomerSignedBy("TestContact");
+        newContractPage.setCustomerSignedTittle("tittle");
+        newContractPage.setCustomerSignedDate("7/15/2021");
+        newContractPage.setPriceBook("Standard");
+        newContractPage.setOwnerExpirationNotice("90 Days");
+        newContractPage.setCompanySignedDate("7/19/2021");
+        newContractPage.setBillingStreet("Billing Street");
+        newContractPage.setBillingCity("Billing City");
+        newContractPage.setBillingStreet("Billing Street");
+        newContractPage.setBillingState("Billing state");
+        newContractPage.setBillingPostalCode("Billing postal code");
+        newContractPage.setBillingCountry("Billing country");
+        newContractPage.setSpecialTerms("SpecialTerms");
+        newContractPage.setDescription("Description");
         createdContractPage = newContractPage.clickSave();
         Assert.assertEquals(createdContractPage.getAccountName(), "TestAccount");
     }
